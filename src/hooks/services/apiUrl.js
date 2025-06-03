89896f3619941bd44133a63f928adf6ecf65a866
Amit Kumar <amit.kaushik@techqware.com>
2025-06-03 14:48:59 +0530

    url change

--- src/hooks/services/apiUrl.js
+++ src/hooks/services/apiUrl.js
@@ -1,15 +1,15 @@
 
 
 //prod
-// export const API_BASE_URL = 'https://backend.h2.doctor';
-// export const API_URL = 'https://backend.h2.doctor';
-// export const WEB_SOCKET_URL = 'wss://backend.h2.doctor';
-// export const LIVE_URL= 'https://h2.doctor'
+export const API_BASE_URL = 'https://backend.h2.doctor';
+export const API_URL = 'https://backend.h2.doctor';
+export const WEB_SOCKET_URL = 'wss://backend.h2.doctor';
+export const LIVE_URL= 'https://h2.doctor'
 
 //stage
-export const API_BASE_URL = 'http://172.167.171.97';
-export const API_URL = 'http://172.167.171.97';
-export const WEB_SOCKET_URL =  'ws://172.167.171.97';
-export const LIVE_URL= 'https://stage.h2.doctor'
+// export const API_BASE_URL = 'http://172.167.171.97';
+// export const API_URL = 'http://172.167.171.97';
+// export const WEB_SOCKET_URL =  'ws://172.167.171.97';
+// export const LIVE_URL= 'https://stage.h2.doctor'
 
 
